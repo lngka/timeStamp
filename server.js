0bd5d85adee4fa3f571388a0829bd1d1c827d852
System <none>
2017-07-28 18:23:31 +0000

    👘🚠 Updated with Glitch

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 'use strict';
 
 const express = require('express');
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
+// allow cross origin call
+app.use(cors());
+
+// allow public files
+app.use('/public', express.static(path.join(__dirname,"public")));
 
 app.get("/", function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
+app.get("/:string", function(req, res) {
+   
+  // access parameter with req.params.<parameter name>
+  var unixDate = Date.parse(req.params.string);
+  
+  // dateObj.toLocaleDateString([locales [, options]])
+  // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var answer = {
+    "unix-time": unixDate,
+    "nomal-time": new Date(unixDate).toLocaleDateString("en-US", options)
+  };
+  res.writeHead(200, {"Content-Type": "JSON"});
+  res.end(JSON.stringify(answer));
 });
 
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }
-})
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Node.js listening ... on ' + process.env.PORT);
 });
 
